d898ac4575806bd481d0920f9447f47761cd9c79
Fatih Gur <fatih.gur26@hotmail.com>
2023-01-22 19:30:00 +0300

    Update BuyMeAKebab.sol

--- contracts/BuyMeAKebab.sol
+++ contracts/BuyMeAKebab.sol
@@ -1,8 +1,8 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-// Switch this to your own contract address once deployed, for bookkeeping!
-// Example Contract Address on Goerli: 0xDBa03676a2fBb6711CB652beF5B7416A53c1421D
+// Contract address on Goerli Testnet: 0x703E0A66D4690D399DD4020764f51f27CcF4A6a7
+
 
 contract BuyMeAKebab {
     // Event to emit when a Memo is created.
@@ -21,16 +21,14 @@ contract BuyMeAKebab {
         string message;
     }
     
-    // Address of contract deployer. Marked payable so that
-    // we can withdraw to this address later.
+    // Contract deployer address
     address payable owner;
 
-    // List of all memos received from Kebab purchases.
+    // List of messages
     Memo[] memos;
 
     constructor() {
-        // Store the address of the deployer as a payable address.
-        // When we withdraw funds, we'll withdraw here.
+        
         owner = payable(msg.sender);
     }
 
@@ -50,7 +48,7 @@ contract BuyMeAKebab {
         // Must accept more than 0 ETH for a Kebab.
         require(msg.value > 0, "can't buy Kebab for free!");
 
-        // Add the memo to storage!
+        // Add message sender's information to array
         memos.push(Memo(
             msg.sender,
             block.timestamp,
@@ -67,9 +65,7 @@ contract BuyMeAKebab {
         );
     }
 
-    /**
-     * @dev send the entire balance stored in this contract to the owner
-     */
+    // Sends total ETH to Deployer's address
     function withdrawTips() public {
         require(owner.send(address(this).balance));
     }
